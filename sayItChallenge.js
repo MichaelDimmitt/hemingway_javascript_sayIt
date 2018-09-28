d7a803d440673b88399451c347202afc2ed7ecfd
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-09-28 18:13:53 -0400

    manipulate function bindings,
    The most complicated way to remove an external variable dependency 😳

--- sayItChallenge.js
+++ sayItChallenge.js
@@ -1,17 +1,22 @@
-// simplified solution for say it challenge, 
+// simplified solution for say it challenge,
 // sayIt('hello')('my')('name')('is')('Arundhati')()
 // output expected: hello my name is Arundhati
 
 // Simplified implementation of Slyvia Plath implementation.
 // Improvement still to be completed: 1) remove global words variable.
-let words = ''
-const say_it = (...args) => args.length > 0 ? ( args.map(word => words+=` ${word}`), say_it) : words 
+const say_it = (...args) => {
+ return args.length > 0 ? say_two('', args) : ''
+}
+const say_two = (words, ...args) => {
+  return args.length > 0 ? ( args.map(word => words+=` ${word}`), say_two.bind(null, words) ) : words
+}
+
 const d = (x) => typeof x === 'string' ? x : x()
 
 console.log(`
   the following commands work:
-  command: d(say_it('hello', 'bob')('my')('name')('is')('Arundhati')('have', 'a', 'nice', 'day')('jumbo', 'shrimp')())
-  output: ${d(say_it('hello', 'bob')('my')('name')('is')('Arundhati')('have', 'a', 'nice', 'day')('jumbo', 'shrimp')())},
+  command: d(say_it('hello')('my')('name')('is')('Arundhati')('have', 'a', 'nice', 'day')('jumbo', 'shrimp')())
+  output: ${d(say_it('hello')('my')('name')('is')('Arundhati')('have', 'a', 'nice', 'day')('jumbo', 'shrimp')())},
 
   command: d(say_it('hello', 'test')('my')('name')('is')('Arundhati'))
   output: ${d(say_it('hello', 'test')('my')('name')('is')('Arundhati'))}
