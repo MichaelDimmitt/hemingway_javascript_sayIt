dba68ab877ef180fbdef54f105d17afc750c6c20
MichaelDimmitt <michaelgdimmitt@gmail.com>
2018-10-04 23:04:45 -0400

    solution: mapOverObjectTerminate terminates if an unterminated function.

--- main_body_of_work.js
+++ main_body_of_work.js
@@ -1,4 +1,10 @@
 // Simplified implementation of Slyvia Plath implementation. From the book: If Hemmingway Wrote Javascript
+const d = (x) => typeof x === 'string' ? x : x()
+
+const mapOverObjectTerminate = (myObject) => (
+  Object.keys(myObject).map( (key, index) => d(myObject[key]) )
+)
+
 const say_it_shortened = (...args) => (
   args.length > 0 ? _say_it_shortened(0, args) : ''
 )
@@ -8,13 +14,14 @@ const _say_it_shortened = (prev_length, ...args) => (
     ? _say_it_shortened.bind(null, args.length, ...args)
     : args.join(' ')
 )
+
 async function run_the_stuff(){
   let inputs = {}
   inputs['anonymousTerminator'] = await say_it_shortened('hello')('my')('jumbo', 'shrimp')()
   inputs['funcNeedsTerminator'] = await say_it_shortened('hello')('my')('jumbo', 'shrimp')
 
   console.log(
-    inputs
+    mapOverObjectTerminate(inputs).join('\n')
   )
 }
 run_the_stuff()
